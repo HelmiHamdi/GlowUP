15553cd79dba08eb09adaabe19b74c85654f98dd
Hamdi Helmi <helmihamdi977@gmail.com>
2026-03-09 12:40:58 +0100

    okitos

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5050/api" : "/api",
+   baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5050/api" : "/api",
   withCredentials: true,
   timeout: 10000,
 });
